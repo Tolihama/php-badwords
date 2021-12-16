5387c3e18faa257e4f1e7fbe39f216193e0cfdab
Tolihama <fra070@gmail.com>
2021-12-16 13:57:31 +0100

    Lunghezza stringhe

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php 
 
+// Stringa di testo di partenza
 $text = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. In nihil necessitatibus aut doloribus aliquam totam laborum. Porro vel, minus molestiae qui officiis, quis aspernatur sint quos tenetur recusandae enim tempore!';
 
 // Frase Completa
-echo 'Frase completa:';
-echo '<br>';
-echo $text;
-
-echo '<br><br>';
+echo '<strong>Frase completa:</strong><br>';
+echo $text.'<br>';
+echo 'Lunghezza: '.strlen($text).'<br><br>';
 
 // Frase censurata
-echo 'Frase censurata:';
-echo '<br>';
-echo str_replace($_GET["censor"], "***", $text);
+$textCensored = str_replace($_GET["censor"], "***", $text);
+echo '<strong>Frase censurata:</strong><br>';
+echo $textCensored.'<br>';
+echo 'Lunghezza: '.strlen($textCensored);
 
 ?>
